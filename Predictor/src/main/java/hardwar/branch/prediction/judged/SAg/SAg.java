4a424f417e264e4a8f24920f874c4b6621a2266d
ElaheMrtz <62252751+elahemrtz@users.noreply.github.com>
2023-06-01 16:59:30 +0330

    Update SAg.java

--- Predictor/src/main/java/hardwar/branch/prediction/judged/SAg/SAg.java
+++ Predictor/src/main/java/hardwar/branch/prediction/judged/SAg/SAg.java
@@ -1,6 +1,5 @@
 package hardwar.branch.prediction.judged.SAg;
 
-
 import hardwar.branch.prediction.shared.*;
 import hardwar.branch.prediction.shared.devices.*;
 
@@ -18,41 +17,79 @@ public class SAg implements BranchPredictor {
     }
 
     public SAg(int BHRSize, int SCSize, int branchInstructionSize, int KSize) {
-        // TODO: complete the constructor
-        this.branchInstructionSize = 0;
-        this.KSize = 0;
+        this.branchInstructionSize = branchInstructionSize;
+        this.KSize = KSize;
 
-        // Initialize the PABHR with the given bhr and Ksize
-        PSBHR = null;
+        // Initialize the PSBHR with the given BHR and K size
+        PSBHR = new RegisterBank(BHRSize, KSize);
 
-        // Initialize the PHT with a size of 2^size and each entry having a saturating counter of size "SCSize"
-        PHT = null;
+        // Initialize the PHT with a size of 2^BHRSize and each entry having a saturating counter of size "SCSize"
+        int phtSize = (int) Math.pow(2, BHRSize);
+        int blockSize = SCSize;
+        PHT = new Cache<>(phtSize, blockSize);
 
-        // Initialize the SC register
-        SC = null;
+        // Initialize the SC register with the given size
+        SC = new ShiftRegister(SCSize);
     }
 
     @Override
     public BranchResult predict(BranchInstruction instruction) {
-        // TODO: complete Task 1
-        return BranchResult.NOT_TAKEN;
+        // Read the branch address
+        Bit[] branchAddress = instruction.getBranchAddress();
+
+        // Get the register bank address line based on the branch address
+        Bit[] rbAddressLine = getRBAddressLine(branchAddress);
+
+        // Read the associated block with the RB address line from the PHT
+        Bit[] phtEntry = PHT.read(rbAddressLine);
+
+        // Load the read block from the cache into the SC register
+        SC.setBits(phtEntry);
+
+        // Return the MSB of the read block or SC register
+        return SC.getBits()[0] == Bit.ONE ? BranchResult.TAKEN : BranchResult.NOT_TAKEN;
     }
 
     @Override
     public void update(BranchInstruction branchInstruction, BranchResult actual) {
-        // TODO: complete Task 2
+        // Read the branch address
+        Bit[] branchAddress = branchInstruction.getBranchAddress();
+
+        // Get the register bank address line based on the branch address
+        Bit[] rbAddressLine = getRBAddressLine(branchAddress);
+
+        // Read the associated block with the RB address line from the PHT
+        Bit[] phtEntry = PHT.read(rbAddressLine);
+
+        // Pass the SC register bits to a saturating counter
+        SaturatingCounter counter = new SaturatingCounter(SC.getLength());
+        counter.setBits(SC.getBits());
+
+        // Update the saturating counter based on the actual branch result
+        if (actual == BranchResult.TAKEN) {
+            counter.increment();
+        } else {
+            counter.decrement();
+        }
+
+        // Save the updated value into the cache via RB address line
+        Bit[] updatedValue = counter.getBits();
+        PHT.write(rbAddressLine, updatedValue);
+
+        // Update the PSBHR with the actual branch result
+        PSBHR.setBits(rbAddressLine, actual == BranchResult.TAKEN ? Bit.ONE : Bit.ZERO);
     }
 
     private Bit[] getRBAddressLine(Bit[] branchAddress) {
-        // hash the branch address
+        // Hash the branch address
         return hash(branchAddress);
     }
 
     /**
-     * hash N bits to a K bit value
+     * Hash N bits to a K bit value.
      *
      * @param bits program counter
-     * @return hash value of fist M bits of `bits` in K bits
+     * @return hash value of first M bits of `bits` in K bits
      */
     private Bit[] hash(Bit[] bits) {
         Bit[] hash = new Bit[KSize];
@@ -65,23 +102,13 @@ public class SAg implements BranchPredictor {
             } else {
                 Bit xorProduce = hash[j].getValue() ^ bits[i].getValue() ? Bit.ONE : Bit.ZERO;
                 hash[j] = xorProduce;
-
             }
         }
         return hash;
     }
 
-    /**
-     * @return a zero series of bits as default value of cache block
-     */
-    private Bit[] getDefaultBlock() {
-        Bit[] defaultBlock = new Bit[SC.getLength()];
-        Arrays.fill(defaultBlock, Bit.ZERO);
-        return defaultBlock;
-    }
-
     @Override
     public String monitor() {
-        return null;
+        return "SAg predictor snapshot:\n" + PSBHR.monitor() + SC.monitor() + PHT.monitor();
     }
 }
